8e2de62d4947b0d96482ed87b68cfc93415fa148
David Szanto <dav.szanto@gmail.com>
2023-01-22 22:22:46 +0100

    Changed a few default settings

--- src/main/java/com/ramen_to_go/mods/more_o_copper/mixin/TitleScreenMixin.java
+++ src/main/java/com/ramen_to_go/mods/more_o_copper/mixin/TitleScreenMixin.java
@@ -1,6 +1,6 @@
-package com.example.example_mod.mixin;
+package com.ramen_to_go.mods.more_o_copper.mixin;
 
-import com.example.example_mod.ExampleMod;
+import com.ramen_to_go.mods.more_o_copper.MoreOCopper;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void exampleMod$onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		MoreOCopper.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
