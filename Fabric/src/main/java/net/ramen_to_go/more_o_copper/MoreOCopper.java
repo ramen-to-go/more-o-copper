04911a9067c3c88a6cf69a67dd2a7cc5824536ed
David Szanto <dav.szanto@gmail.com>
2023-01-23 16:06:07 +0100

    changed folders

---
+++ Fabric/src/main/java/net/ramen_to_go/more_o_copper/MoreOCopper.java
@@ -0,0 +1,33 @@
+package net.ramen_to_go.more_o_copper;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoreOCopper implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("more_o_copper");
+
+	// Add Super Copper
+	public static final Item SUPERCOPPER_INGOT = 
+		Registry.register(Registries.ITEM, new Identifier("more_o_copper", "supercopper_ingot"), 
+			new Item(new FabricItemSettings()));
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+
+	}
+}
